e9331bb218b36369f0cd0f8248a2613a8b489de4
zaehyeon2 <insam2802@gmail.com>
2023-10-12 23:46:34 +0900

    feat: 특성치

--- src/pages/Cthulu-Generator.tsx
+++ src/pages/Cthulu-Generator.tsx
@@ -1,32 +1,265 @@
-import { Card, Grid } from '@mantine/core';
-import React from 'react';
+import { Card, Container, Grid, Group, Stack, Text, TextInput } from '@mantine/core';
+import React, { useEffect, useState } from 'react';
 import { Stats } from '../components/stats';
+import { explorerStyles } from '../styles/styles';
+import { isNumber } from '../services/utils.service';
+
+interface Stats {
+  job: string;
+  age: number;
+  str: number;
+  dex: number;
+  int: number;
+  health: number;
+  appeareance: number;
+  mentality: number;
+  size: number;
+  education: number;
+  mobility: number;
+  luck: number;
+}
 
 export function CthulhuGenerator() {
+  const { classes } = explorerStyles();
+  const [statValues, setStatsValue] = useState({
+    age: 0,
+    str: 0,
+    dex: 0,
+    int: 0,
+    health: 0,
+    appeareance: 0,
+    mentality: 0,
+    size: 0,
+    education: 0,
+    mobility: 0,
+    luck: 0,
+  } as Stats)
+
+  function getAndSetValue(key: string, value: number) {
+    setStatsValue({ ...statValues, [key]: value });
+  }
+
+  function getMobility() {
+    const { str, dex, size, age } = statValues;
+
+    let mobility: number;
+    if (str < size && dex < size) mobility = 7;
+    else if (str > size && dex > size) mobility = 9;
+    else mobility = 8;
+    if (age >= 80) mobility -= 5;
+    else if (age >= 70) mobility -= 4;
+    else if (age >= 60) mobility -= 3;
+    else if (age >= 50) mobility -= 2;
+    else if (age >= 40) mobility -= 1;
+
+    setStatsValue({ ...statValues, mobility });
+  }
+
+  useEffect(() => {
+    getMobility();
+  }, [statValues.str, statValues.dex, statValues.size, statValues.age]);
+
   return (
     <Card withBorder radius="md">
       <Grid justify="center" align="center">
-        <Grid.Col span={3}>
-          <Stats value={10} maxValue={90} isClass />
+        <Grid.Col span={4}>
+          <Stack spacing='xs' sx={{ border: "solid", paddingBottom: '10px', height: "330px" }}>
+            <Text sx={{ backgroundColor: "black", width: '100%' }}>현대 탐사자</Text>
+            <Group sx={{ margin: 'auto' }}>
+              <Text className={classes.label}>이름</Text>
+              <TextInput size="xs" />
+            </Group>
+            <Group sx={{ margin: 'auto' }}>
+              <Text className={classes.label}>플레이어</Text>
+              <TextInput size="xs" />
+            </Group>
+            <Group sx={{ margin: 'auto' }}>
+              <Text className={classes.label}>직업</Text>
+              <TextInput value={statValues.job} size="xs" onChange={(event) => {
+                setStatsValue({ ...statValues, job: event.currentTarget.value })
+              }} />
+            </Group>
+            <Group sx={{ margin: 'auto' }}>
+              <Text className={classes.label}>나이</Text>
+              <TextInput value={statValues.age} size="xs" onChange={(event) => {
+                if (!isNumber(event.currentTarget.value)) return;
+                setStatsValue({ ...statValues, age: +event.currentTarget.value })
+              }} />
+            </Group>
+            <Group sx={{ margin: 'auto' }}>
+              <Text className={classes.label}>성별</Text>
+              <TextInput size="xs" />
+            </Group>
+            <Group sx={{ margin: 'auto' }}>
+              <Text className={classes.label}>거주지</Text>
+              <TextInput size="xs" />
+            </Group>
+            <Group sx={{ margin: 'auto' }}>
+              <Text className={classes.label}>출생지</Text>
+              <TextInput size="xs" />
+            </Group>
+          </Stack>
         </Grid.Col>
-        <Grid.Col span={3}>
-          <Stats value={10} label="건강" maxValue={90} />
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Stats
-            value={0}
-            label="건강"
-            nDices={2}
-            nSides={6}
-            baseValue={6}
-            multiplyValue={5}
-            maxValue={99}
-          />
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Stats value={10} label="건강" nDices={1} nSides={100} maxValue={90} />
+        <Grid.Col span={8}>
+          <Stack justify="space-between" spacing='xs' sx={{ paddingBottom: '10px', border: "solid", height: "330px" }}>
+            <Text sx={{ backgroundColor: "brown" }}>특성치</Text>
+            <Grid justify="center" align="center">
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="str"
+                  value={statValues.str}
+                  label="근력"
+                  nDices={3}
+                  nSides={6}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="dex"
+                  value={statValues.dex}
+                  label="민첩성"
+                  nDices={3}
+                  nSides={6}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="int"
+                  value={statValues.int}
+                  label="지능"
+                  nDices={2}
+                  nSides={6}
+                  baseValue={6}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+            </Grid>
+            <Grid justify="center" align="center">
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="health"
+                  value={statValues.health}
+                  label="건강"
+                  nDices={3}
+                  nSides={6}
+                  baseValue={0}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="appeareance"
+                  value={statValues.appeareance}
+                  label="외모"
+                  nDices={3}
+                  nSides={6}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="mentality"
+                  value={statValues.mentality}
+                  label="정신력"
+                  nDices={3}
+                  nSides={6}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+            </Grid>
+            <Grid justify="center" align="center">
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="size"
+                  value={statValues.size}
+                  label="크기"
+                  nDices={2}
+                  nSides={6}
+                  baseValue={6}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <Stats
+                  statKey="education"
+                  value={statValues.education}
+                  label="교육"
+                  nDices={2}
+                  nSides={6}
+                  baseValue={6}
+                  multiplyValue={5}
+                  maxValue={99}
+                  getAndSetFunction={getAndSetValue}
+                />
+              </Grid.Col>
+              <Grid.Col span={4} >
+                <Container>
+                  <Stack sx={{ border: '1px solid', borderRadius: '0.5em' }} justify="center">
+                    <Text>이동력</Text>
+                    <Text>{statValues.mobility}</Text>
+                  </Stack>
+                </Container>
+              </Grid.Col>
+            </Grid>
+          </Stack>
         </Grid.Col>
       </Grid>
-    </Card>
+      <Stack justify="space-between" spacing='xs' sx={{ paddingBottom: '10px', paddingTop: '10px', border: "solid", marginTop: "16px" }}>
+        <Grid justify="center" align="center">
+          <Grid.Col span={3}>
+            <Container>
+              <Stack sx={{ border: '1px solid', borderRadius: '0.5em' }} justify="center">
+                <Text>체력</Text>
+                <Text>{Math.floor((statValues.size + statValues.health) / 10)}</Text>
+              </Stack>
+            </Container>
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <Stats
+              statKey="luck"
+              value={statValues.luck}
+              label="운"
+              nDices={3}
+              nSides={6}
+              multiplyValue={5}
+              maxValue={99}
+              getAndSetFunction={getAndSetValue}
+            />
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <Container>
+              <Stack sx={{ border: '1px solid', borderRadius: '0.5em' }} justify="center">
+                <Text>이성</Text>
+                <Text>{statValues.mentality}</Text>
+              </Stack>
+            </Container>
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <Container>
+              <Stack sx={{ border: '1px solid', borderRadius: '0.5em' }} justify="center">
+                <Text>마력</Text>
+                <Text>{Math.floor(statValues.mentality / 5)}</Text>
+              </Stack>
+            </Container>
+          </Grid.Col>
+        </Grid>
+      </Stack>
+    </Card >
   );
 }
