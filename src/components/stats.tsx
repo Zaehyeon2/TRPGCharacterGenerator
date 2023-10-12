83355fd3752bb36ec0c5dc2fcdbfed04ddf886a4
Zaehyeon2 <insam2802@gmail.com>
2023-10-12 18:15:15 +0900

    feat: add stats class traits

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/require-default-props */
-import { Text, Stack, Grid, TextInput, Button, UnstyledButton, Group } from '@mantine/core';
+import { Text, Stack, Grid, TextInput, UnstyledButton, Checkbox, Container } from '@mantine/core';
 import React, { useState } from 'react';
 import { rollDice } from '../services/dice.service';
 
@@ -8,6 +8,7 @@ interface StatsProps {
   valueAddedBaseValue: number;
   valueDividedBy2: number;
   valueDividedBy5: number;
+  isClassTraits: boolean;
 }
 
 interface StatsParams {
@@ -17,14 +18,24 @@ interface StatsParams {
   nSides?: number;
   baseValue?: number;
   maxValue: number;
+  isClass?: boolean;
 }
 
-export function Stats({ label, value, nDices, nSides, baseValue = 0, maxValue }: StatsParams) {
+export function Stats({
+  label,
+  value,
+  nDices,
+  nSides,
+  baseValue = 0,
+  maxValue,
+  isClass = false,
+}: StatsParams) {
   const [statValues, setStatValues] = useState<StatsProps>({
     value,
     valueAddedBaseValue: Math.min(maxValue, value + baseValue),
     valueDividedBy2: Math.floor(Math.min(maxValue, value + baseValue) / 2),
     valueDividedBy5: Math.floor(Math.min(maxValue, value + baseValue) / 5),
+    isClassTraits: false,
   });
 
   function setStats(stat: number) {
@@ -34,6 +45,7 @@ export function Stats({ label, value, nDices, nSides, baseValue = 0, maxValue }:
       valueAddedBaseValue,
       valueDividedBy2: Math.floor(valueAddedBaseValue / 2),
       valueDividedBy5: Math.floor(valueAddedBaseValue / 5),
+      isClassTraits: statValues.isClassTraits,
     });
   }
 
@@ -44,42 +56,55 @@ export function Stats({ label, value, nDices, nSides, baseValue = 0, maxValue }:
   }
 
   return (
-    <Stack align="center" spacing={0} sx={{ border: '1px solid', borderRadius: '0.5em' }}>
-      <Text align="center" fz="sm">
-        {label}
-      </Text>
-      <Grid justify="center" align="center" sx={{ padding: '5px' }}>
-        <Grid.Col span={5}>
-          <TextInput
-            value={statValues.value}
-            onChange={(event) => {
-              const numberedValue = Number(event.currentTarget.value);
-              if (Number.isNaN(numberedValue) || numberedValue < 0) return;
-              setStats(numberedValue);
-            }}
-          />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Grid justify="center" align="center">
-            <Grid.Col span={6}>
-              <Text fz="xl">{statValues.valueAddedBaseValue}</Text>
+    <Container>
+      <Stack align="center" spacing={0} sx={{ border: '1px solid', borderRadius: '0.5em' }}>
+        <Text align="center" fz="sm">
+          {label}
+        </Text>
+        <Grid justify="center" align="center" sx={{ paddingTop: '5px', paddingBottom: '5px' }}>
+          {isClass && (
+            <Grid.Col span="content">
+              <Checkbox
+                checked={statValues.isClassTraits}
+                size="xs"
+                onChange={() => {
+                  setStatValues({ ...statValues, isClassTraits: !statValues.isClassTraits });
+                }}
+              />
             </Grid.Col>
-            <Grid.Col span={6}>
-              <Stack spacing={0} align="center">
-                <Text fz="xs">{statValues.valueDividedBy2}</Text>
-                <Text fz="xs">{statValues.valueDividedBy5}</Text>
-              </Stack>
-            </Grid.Col>
-          </Grid>
-        </Grid.Col>
-        {nDices && nSides && (
-          <Grid.Col span={3}>
-            <UnstyledButton onClick={() => rollStat()}>
-              <img src="/src/assets/dice20.png" alt="roll" width="20px" />
-            </UnstyledButton>
+          )}
+          <Grid.Col span={5}>
+            <TextInput
+              value={statValues.value}
+              onChange={(event) => {
+                const numberedValue = Number(event.currentTarget.value);
+                if (Number.isNaN(numberedValue) || numberedValue < 0) return;
+                setStats(numberedValue);
+              }}
+            />
           </Grid.Col>
-        )}
-      </Grid>
-    </Stack>
+          <Grid.Col span={4}>
+            <Grid justify="center" align="center">
+              <Grid.Col span={6}>
+                <Text fz="xl">{statValues.valueAddedBaseValue}</Text>
+              </Grid.Col>
+              <Grid.Col span={5}>
+                <Stack spacing={0} align="center">
+                  <Text fz="xs">{statValues.valueDividedBy2}</Text>
+                  <Text fz="xs">{statValues.valueDividedBy5}</Text>
+                </Stack>
+              </Grid.Col>
+            </Grid>
+          </Grid.Col>
+          {nDices && nSides && (
+            <Grid.Col span="content">
+              <UnstyledButton onClick={() => rollStat()}>
+                <img src="/src/assets/dice20.png" alt="roll" width="20px" />
+              </UnstyledButton>
+            </Grid.Col>
+          )}
+        </Grid>
+      </Stack>
+    </Container>
   );
 }
--- src/pages/Cthulu-Generator.tsx
+++ src/pages/Cthulu-Generator.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid } from '@mantine/core';
+import { Card, Grid, Container } from '@mantine/core';
 import React from 'react';
 import { Stats } from '../components/stats';
 
@@ -7,7 +7,7 @@ export function CthulhuGenerator() {
     <Card withBorder radius="md">
       <Grid justify="center" align="center">
         <Grid.Col span={3}>
-          <Stats value={10} nDices={1} nSides={100} maxValue={90} />
+          <Stats value={10} maxValue={90} isClass />
         </Grid.Col>
         <Grid.Col span={3}>
           <Stats value={10} label="건강" baseValue={10} maxValue={90} />
