eca709a8847467e69e09cc35feafdd02ad6209f1
Zaehyeon2 <insam2802@gmail.com>
2026-01-14 18:53:42 +0900

    feat: 모바일에서 햄버거 메뉴 추가 (1280px 미만)

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,17 +1,40 @@
-import { Header, UnstyledButton, Group, Text } from '@mantine/core';
+import { Header, UnstyledButton, Group, Text, Burger, Drawer, ScrollArea } from '@mantine/core';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import dice20 from '../assets/dice20.png';
+import { MainLinks } from './_NavLinks';
+import { FooterSimple } from './Footer';
 
 export function HeaderSimple() {
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const isDesktop = useMediaQuery('(min-width: 1280px)');
+
   return (
-    <Header height={80} p="md">
-      <UnstyledButton component={Link} to="/">
-        <Group position="center" mt="md">
-          <img src={dice20} alt="dice20" width={26.6} />
-          <Text size="lg">TRPG Character Generators</Text>
+    <>
+      <Header height={80} p="md">
+        <Group position="apart" noWrap sx={{ height: '100%' }}>
+          {!isDesktop && <Burger opened={opened} onClick={toggle} size="sm" />}
+          <UnstyledButton component={Link} to="/" sx={{ flex: 1 }}>
+            <Group position="center" noWrap>
+              <img src={dice20} alt="dice20" width={26.6} />
+              <Text size="lg" sx={{ whiteSpace: 'nowrap' }}>
+                TRPG Character Generators
+              </Text>
+            </Group>
+          </UnstyledButton>
+          {!isDesktop && <div style={{ width: 34 }} />}
         </Group>
-      </UnstyledButton>
-    </Header>
+      </Header>
+
+      <Drawer opened={opened} onClose={close} size="250px" padding="md" title="Menu" zIndex={1000}>
+        <ScrollArea sx={{ height: 'calc(100vh - 140px)' }}>
+          <div onClick={close}>
+            <MainLinks />
+          </div>
+        </ScrollArea>
+        <FooterSimple />
+      </Drawer>
+    </>
   );
 }
